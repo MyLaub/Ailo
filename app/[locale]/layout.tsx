2e89c3880c75fd8e985e9b40259a9310eb386d9e
My <idla0003@stud.kea.dk>
2026-03-12 12:12:26 +0100

    feat: add internationalization support with next-intl
    - Created a new home.json file for English translations.
    - Updated package-lock.json and package.json to include next-intl version 4.8.3.
    - Added proxy.ts to set up middleware for internationalization routing.
    - Modified tsconfig.json to set baseUrl for module resolution.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
-import Header from "./global_components/header";
+import Header from "../global_components/header";
 
 // importing dm-sans font from google fonts
 const dmSans = DM_Sans({
@@ -19,10 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="da">
-      <body className={`${dmSans.variable} antialiased`}>
-
-        {children}
-      </body>
+      <body className={`${dmSans.variable} antialiased`}>{children}</body>
     </html>
   );
-} 
+}
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
-import WhatWeDo from "./sections_index/whatwedo";
-import WhoWeAre from "./sections_index/whoweare";
-
+import WhatWeDo from "../sections_index/whatwedo";
+import WhoWeAre from "../sections_index/whoweare";
 
 export default function Home() {
   return (
